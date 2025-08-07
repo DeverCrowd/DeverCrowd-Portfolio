44ea47c359803927f80162b535bc22a1ffbcf44f
Mohamed Hammad <midoeljoker30@gmail.com>
2025-08-07 18:44:38 +0300

    a

--- frontend/data/dynamic/testimonials.js
+++ frontend/data/dynamic/testimonials.js
@@ -2,7 +2,7 @@ const testimonials = [
   {
     name: "Mohamed Ali",
     image: "/icons/user.webp",
-    test: "حصلت علي خدمة حجز دومين و الخدمه ممتازه و تم التسليم في الوقت المناسب و الجوده بشكل عام جيدة",
+    test: "I received a domain registration service, and it was excellent. Delivery was on time, and the overall quality was good.",
   },
 ];
 
