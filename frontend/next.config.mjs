b9e4b78f2234b41d27dc79b6cd5613a3cc3b312a
mohamed-swilam <midoeljoker30@gmail.com>
2026-04-13 02:50:06 +0200

    Enhance Next.js configuration and admin login page
    - Updated next.config.mjs to use absolute paths for turbopack root.
    - Added dynamic rendering to the admin login page for improved performance.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,16 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: './',
+    root: path.resolve(__dirname, '../'), // absolute path
   },
   images: {
     remotePatterns: [
@@ -24,4 +29,5 @@ const nextConfig = {
     ],
   },
 };
+
 export default withNextIntl(nextConfig);
